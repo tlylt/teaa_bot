e060acdbc76f87d3addb5ca8f705f3cb395bd4d9
tlylt <liu_yongliang@hotmail.com>
2022-01-08 02:02:46 +0800

    Hack and roll
    Co-authored-by: Junx <ong6@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,7 +1,74 @@
+## Tea A Bot
+Team
+- Jun Xiong
+- Yongliang
+
+## Flow
+1. The bot is deployed in the cloud and will be available after a command is issued to it.
+2. Users either chat with the bot or add the bot into a telegram group.
+3. Admin send a specific command to the bot.
+4. The bot will respond to the command and send a series of quiz questions to the chat or telegram group.
+5. The user will answer the quiz questions.
+
+## User Stories
+- As an admin, I want a command to trigger the bot to send out quiz questions into the group.
+- As an admin, I want a way to store the quiz questions in the backend.
+- As an admin, I want a way to organize the quiz questions into weeks.
+- As an user, I want to be able to view the code in the quiz with syntax highlighting.
+- Bonus: the users can submit a question to the bot for consideration.
+
+## Development
+1. git clone the repository (either via GitHub desktop or CLI)
+2. change to project directory
+ - `cd teaa_bot` 
+3. create virtual environment
+ - `python3 -m venv venv # If not created, creating virtualenv`
+4. activate virtual environment
+ - Windows: `venv\Scripts\activate.bat`
+ - Mac: `source ./venv/bin/activate # Activating virtualenv`
+ - (Better) using VSCode, select the python interpreter within the `venv` folder and the above is done automatically
+5. install dependencies
+ - `pip3 install -r ./requirements.txt # Installing dependencies`
+6. to update dependencies requirements
+ - `pip freeze > requirements.txt`
 ## Commands
-- To re-deploy: `git push heroku main`
+- To re-deploy: `git push heroku main` (For the one with the hosting account)
+- To start the bot locally: `python bot.py`
 ## Reference
 - [Creating a Telegram Chatbot Quiz with Python](https://towardsdatascience.com/creating-a-telegram-chatbot-quiz-with-python-711a43c0c424)
 - [Bring your Telegram Chatbot to the next level](https://towardsdatascience.com/bring-your-telegram-chatbot-to-the-next-level-c771ec7d31e4)
 - [Deployment](https://towardsdatascience.com/how-to-deploy-a-telegram-bot-using-heroku-for-free-9436f89575d2)
 - [python-telegram-bot on Heroku](https://github.com/Bibo-Joshi/ptb-heroku-skeleton)
+
+
+## Inspiration
+As TA's (teach assistants) we feel that it is very important to keep students engaged in the learning process. One of these methods to engage students is to ask them questions, and what better way to achieve this than by automating it using a telegram bot!
+
+## What it does
+Our bot houses a large library of questions from which the TA's can pick from. All they have to do is to add the bot to their teaching telegram group and voilà. It just works. With 2 simple commands, the user can browse through the list of quiz sets we have in our library as well as the questions in them.
+
+## Functions
+`/help` - brings up a help list
+`/quiz [quiz_name]` - sends the quiz with quiz name specified (restricted to people with access only)
+`/list` - lists all the quizzes
+`/sample` - sample question to see how the bot sends
+`/submit` - submit a sample question
+
+## How we built it
+We built this app with a python backend and hosted it on Heroku
+
+## Challenges we ran into
+Reading the bad documentation that plagues most python libraries 
+
+## Accomplishments that we're proud of
+A robot friend to help busy TAs manage and send short quizzes to their students.
+
+## What we learned
+Telegram truly is a powerful tool and their API enables many possibilities.
+
+## What's next for Tea A Bot
+More features:
+- Support for multiple modules
+- Support for more question types (possibly open-ended)
+- Support for out of telegram quiz library UI
+
--- bot.py
+++ bot.py
@@ -1,22 +1,37 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Poll
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    CallbackQueryHandler,
-    PollHandler,
-)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
 import time
 import os
 from dotenv import load_dotenv, find_dotenv
 import os
+import json
+import random
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import ImageFormatter
+from functools import wraps
+
 load_dotenv(find_dotenv())
+
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in json.loads(os.environ.get("ADMINS", "")):
+            print("Unauthorized access denied for {}.".format(user_id))
+            update.message.reply_text(
+                "Only admins can do that!\nContact @Jun_Xiong or @lyongl for help."
+            )
+            return
+        return func(update, context, *args, **kwargs)
+
+    return wrapped
+
 
 def get_chat_id(update, context):
     chat_id = -1
-
     if update.message is not None:
         chat_id = update.message.chat.id
     elif update.callback_query is not None:
@@ -28,7 +43,102 @@ def get_chat_id(update, context):
 
 def help_command_handler(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text("Type /start")
+    update.message.reply_text(
+        "Type /list to list all quizzes\nType /quiz x to send a quiz"
+    )
+
+
+def sample_command_handler(update, context):
+    update.message.reply_text("Sample Quiz Incoming!")
+    send_quiz(update, context, 1)
+
+
+@restricted
+def deploy_quiz_command_handler(update, context):
+    """Send a message when the command /help is issued."""
+    if not context.args:
+        update.message.reply_text("Please provide a quiz index\nE.g. /quiz 1")
+        return
+    update.message.reply_text("Quiz Incoming!")
+    try:
+        send_quiz(update, context, context.args[0])
+    except Exception as e:
+        print(e)
+
+
+def list_quiz_command_handler(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text("Quiz List Incoming!")
+    # get all file names from data
+    files = os.listdir("data")
+    files = [f.split(".")[0] for f in files]
+    files = [f for f in files if f.isdigit()]
+    files.sort()
+    files = [f + ".json" for f in files]
+    # send list of files
+    for f in files:
+        # read file
+        with open(f"data/{f}") as fp:
+            data = json.load(fp)
+            # send file name
+            file_qn = (
+                "Quiz name:  "
+                + f.split(".")[0]
+                + "   \(Command to send: \\\quiz "
+                + f.split(".")[0]
+                + "\)\n"
+            )
+            # send file content
+            for idx in range(len(data)):
+                file_qn += escape_dot(
+                    "\nQuestion " + str(idx + 1) + ": " + data[idx].get("question")
+                )
+            add_text_message(update, context, file_qn)
+        # update.message.reply_text(f)
+
+
+def check_confimation(update, context):
+    add_text_message(update, context, escape_dot("y/n?"))
+    confirmation = update.message.text
+    if confirmation == "y":
+        add_text_message(update, context, "Confirmed...")
+        return True
+    else:
+        add_text_message(update, context, "Exitting...")
+        return False
+
+
+def reply_value(update, context):
+    value = update.message.text
+    add_typing(update, context)
+    add_text_message(update, context, escape_dot("You inputed: " + value))
+
+
+def submit_question_command_handler(update, context):
+    update.message.reply_text(
+        "Submit question through this link: \nhttps://forms.gle/aLssGK6QMekTR75n9"
+    )
+    # add_text_message(update, context, escape_dot("Enter a Question (required):"))
+    # reply_value(update, context)
+    # if not check_confimation(update, context):
+    #     return
+    # add_text_message(update, context, "Number of options? (required):")
+    # reply_value(update, context)
+
+    # add_text_message(update, context, "Option 1:")
+
+    # add_text_message(update, context, "Add an explnation (default:none):")
+
+
+def escape_dot(dotted_string):
+    dotted_string = dotted_string.replace(".", "\.")
+    dotted_string = dotted_string.replace("(", "\(")
+    dotted_string = dotted_string.replace(")", "\)")
+    dotted_string = dotted_string.replace("*", "\*")
+    dotted_string = dotted_string.replace("_", "\_")
+    dotted_string = dotted_string.replace("`", "\`")
+    dotted_string = dotted_string.replace("~", "\~")
+    return dotted_string
 
 
 def get_text_from_callback(update):
@@ -45,60 +155,128 @@ def add_typing(update, context):
 
 
 def add_text_message(update, context, message):
-    context.bot.send_message(chat_id=get_chat_id(update, context), text=message)
+    context.bot.send_message(
+        chat_id=get_chat_id(update, context),
+        text=message,
+        parse_mode=telegram.ParseMode.MARKDOWN_V2,
+    )
 
-def main_handler(update, context):
+
+def code_to_img(code):
+    return highlight(code, PythonLexer(), ImageFormatter())
+
+
+def send_code(context, chat_id, qn):
+    if qn.get("code"):
+        context.bot.send_photo(
+            chat_id=chat_id,
+            photo=code_to_img(qn.get("code")),
+        )
+
+
+def mcq(update, context, chat_id, qn):
+    options = qn["correct_answers"] + qn["incorrect_answers"]
+    random.shuffle(options)
+    ans = 0
+    for idx in range(len(options)):
+        if options[idx] in qn["correct_answers"]:
+            ans = idx
+    context.bot.send_poll(
+        chat_id=chat_id,
+        question=qn["question"],
+        options=options,
+        type=Poll.QUIZ,
+        correct_option_id=ans,
+        explanation=qn["explanation"],
+    )
+
+
+def mrq(update, context, chat_id, qn):
+    options = qn["correct_answers"] + qn["incorrect_answers"]
+    random.shuffle(options)
+    ans = []
+    for idx in range(len(options)):
+        if options[idx] in qn["correct_answers"]:
+            ans.append(str(idx + 1))
+    context.bot.send_poll(
+        chat_id=chat_id,
+        question=qn["question"],
+        options=options,
+        type=Poll.REGULAR,
+        allows_multiple_answers=True,
+    )
+    ans_text = "The correct answers are:\n||"
+    ans_text += ", ".join(ans)
+    ans_text += "||"
+
+    explanation_text = "The explanation:\n||" + qn["explanation"] + "||"
+
+    add_text_message(update, context, ans_text)
+    add_text_message(update, context, explanation_text)
+
+
+def send_quiz(update, context, qn_number):
+    c_id = get_chat_id(update, context)
+    with open(f"data/{qn_number}.json") as f:
+        data = json.load(f)
+        for qn in data:
+            send_code(context, c_id, qn)
+            if qn["type"] == "MCQ":
+                mcq(update, context, c_id, qn)
+            elif qn["type"] == "MRQ":
+                mrq(update, context, c_id, qn)
+            else:
+                raise Exception("Unknown question type")
+
+
+def echo(update, context):
     if update.message is not None:
         user_input = update.message.text
         # reply
         add_typing(update, context)
-        add_text_message(update, context, f"You said: {user_input}")
-    c_id = get_chat_id(update, context)
-    q = 'Question'
-    answers = ['Rome', 'London', 'Amsterdam']
-    context.bot.send_poll(chat_id=c_id, question=q, options=answers, type=Poll.QUIZ, correct_option_id=0)
+        add_text_message(update, context, f"You said: {user_input} ||spoiler||")
+        add_text_message(update, context, "test\!")
 
 
-
-# def poll_handler(update, context):
-#     logging.info(f"question : {update.poll.question}")
-#     logging.info(f"correct option : {update.poll.correct_option_id}")
-#     logging.info(f"option #1 : {update.poll.options[0]}")
-#     logging.info(f"option #2 : {update.poll.options[1]}")
-#     logging.info(f"option #3 : {update.poll.options[2]}")
-
-#     user_answer = get_answer(update)
-#     logging.info(f"correct option {is_answer_correct(update)}")
-
-#     add_typing(update, context)
-#     add_text_message(update, context, f"Correct answer is {user_answer}")
-
+def main_handler(update, context):
+    if update.message is not None:
+        user_input = update.message.text
+        if user_input == "yolo":
+            add_typing(update, context)
+            add_text_message(update, context, "YOLO")
 
 
 def main():
-    updater = Updater(
-        os.environ.get("TELEGRAM_TOKEN", ""), use_context=True
-    )
+    updater = Updater(os.environ.get("TELEGRAM_TOKEN", ""), use_context=True)
     dp = updater.dispatcher
     # command handlers
     dp.add_handler(CommandHandler("help", help_command_handler))
+    dp.add_handler(CommandHandler("list", list_quiz_command_handler))
+    dp.add_handler(CommandHandler("quiz", deploy_quiz_command_handler))
+    dp.add_handler(CommandHandler("submit", submit_question_command_handler))
+    dp.add_handler(CommandHandler("sample", sample_command_handler))
+
     # message handler
     dp.add_handler(MessageHandler(Filters.text, main_handler))
-    # quiz handler
-    #   dp.add_handler(PollHandler(poll_handler, pass_chat_data=True, pass_user_data=True))
-    # start polling
-    # updater.start_polling()
-    # updater.idle()
 
-    # Start the webhook
-    TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
-    NAME = os.environ.get("NAME", "")
-    PORT = int(os.environ.get('PORT', 5000))
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN,
-                          webhook_url=f"https://{NAME}.herokuapp.com/{TOKEN}")
-    updater.idle()
+    ENV = os.environ.get("ENV", "")
+    if ENV == "production":
+        # Start the webhook
+        TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
+        NAME = os.environ.get("NAME", "")
+        PORT = int(os.environ.get("PORT", 5000))
+        updater.start_webhook(
+            listen="0.0.0.0",
+            port=int(PORT),
+            url_path=TOKEN,
+            webhook_url=f"https://{NAME}.herokuapp.com/{TOKEN}",
+        )
+        updater.idle()
+    elif ENV == "development":
+        # start polling
+        updater.start_polling()
+        updater.idle()
+
 
 if __name__ == "__main__":
     load_dotenv(find_dotenv())
